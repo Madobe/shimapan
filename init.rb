ad5150e90e54ecaec631b377fa0b4bfa703f92a6
Madobe <sin_emerald@hotmail.com>
2017-03-21 18:23:17 -0700

    Add the moderation commands (except mute)

--- init.rb
+++ init.rb
@@ -16,12 +16,33 @@ custom_commands.extend FileManagement::CustomCommands
 
 bot = Discordrb::Commands::CommandBot.new token: connection_config['token'], client_id: connection_config['client_id'], prefix: '!', help_command: false, ignore_bots: true
 
+# Kicks the mentioned user out of the server.
+# @param user [String] Must be a mention or ID.
+bot.command(:kick, required_permissions: [:kick_members], usage: '!kick <user>', min_args: 1) do |event, user|
+  user_id = user.gsub(/\D/, '')
+  event.server.kick(user_id)
+end
+
+# Bans the mentioned user from the server.
+# @param user [String] Must be a mention or ID.
+bot.command(:ban, required_permissions: [:ban_members], usage: '!ban <user>', min_args: 1) do |event, user|
+  user_id = user.gsub(/\D/, '')
+  event.server.ban(user_id)
+end
+
+# Unbans the mentioned user from the server.
+# @param user [String] Must be a mention or ID.
+bot.command(:unban, required_permissions: [:ban_members], usage: '!unban <user>', min_args: 1) do |event, user|
+  user_id = user.gsub(/\D/, '')
+  event.server.unban(user_id)
+end
+
 # Add a custom command.
-# @param trigger [String] The custom command trigger.
-# @param output [String] The output of the custom command.
+# @param trigger [string] the custom command trigger.
+# @param output [string] the output of the custom command.
 bot.command(:addcom, required_permissions: [:manage_messages], usage: '!addcom <trigger> <output>', min_args: 2) do |event, trigger, output|
   if custom_commands[trigger]
-    event.respond "`#{trigger}` already exists as a custom command. Maybe you meant to edit the current one?"
+    event.respond "`#{trigger}` already exists as a custom command. maybe you meant to edit the current one?"
   end
 
   custom_commands.update(trigger, output)
