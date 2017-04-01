aa93fe85f6420186b6b8f734c748eb1d40521b94
Madobe <sin_emerald@hotmail.com>
2017-04-01 03:26:18 -0700

    Use String#dump

--- lib/datatype/dbtable.rb
+++ lib/datatype/dbtable.rb
@@ -86,7 +86,7 @@ class DBTable
     attributes.each do |attribute|
       unless send(attribute).nil?
         fields << attribute.to_s
-        values << "%s" % send(attribute)
+        values << send(attribute)
       end
     end
     query.fields = fields
