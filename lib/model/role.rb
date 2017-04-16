94db557507225042d99c0aa8d14582e406c7f973
Madobe <sin_emerald@hotmail.com>
2017-04-16 03:27:49 -0700

    Stop writing duplicate roles into the database

--- lib/model/role.rb
+++ lib/model/role.rb
@@ -2,4 +2,5 @@ require 'active_record'
 
 class Role < ActiveRecord::Base
   validates_presence_of :server_id, :user_id, :role_id
+  validates_uniqueness_of :role_id, scope: [:server_id, :user_id]
 end
